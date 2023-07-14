738259df68c16ddb6c46eee2f1ca80f9461d39d4
JulienBIDIAS <118311425+JulienBIDIAS@users.noreply.github.com>
2023-07-14 19:31:18 +0000

    Update JULIEN.py

--- JULIEN.py
+++ JULIEN.py
@@ -44,9 +44,11 @@ fig3.update_layout(
      title="Produit par sexe")
 st.plotly_chart(fig3)
 
-fig4 = px.pie(df, x='gender')
+fig4 = px.density_contour(df, x='gender', y = 'product_id')
 fig4.update_layout(
-     title="Produit par sexe")
+    xaxis_title='gender',
+    yaxis_title="produits",
+    title="Produit par sexe")
 st.plotly_chart(fig4)
 
 
